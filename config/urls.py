005e2c6ba8db02006fa4814090c3d375e5fa955b
berestw <paladin.berestw@yandex.ru>
2024-08-19 21:21:46 +0300

    Добавил хэширование создаваемого при регистрации пароля

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("users.urls", namespace="users")),
+    path("", include("users.urls", namespace="users")),
     path(
         "",
         include(
